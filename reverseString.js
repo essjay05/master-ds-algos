718485aa4906574c93c49b44da04a1f545b6e9ce
Joy S <stepjoyous@gmail.com>
2023-12-19 21:56:29 -0800

    Created solution 3 for reverseString.

--- reverseString.js
+++ reverseString.js
@@ -3,17 +3,32 @@
 // 'ierdnA si eman yM iH'
 
 const reverseStr = (str) => {
+
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'Not applicable to provided str'
+  }
   // Solution 1:
   // const reversed = str.split('').reverse().join('')
   // return reversed
 
-  // Solution 2:
-  let reversed = []
-  for (let s of str) {
-    // console.log(s)
-    reversed = (s + reversed)
+  // // Solution 2:
+  // let reversed = []
+  // for (let s of str) {
+  //   // console.log(s)
+  //   reversed = (s + reversed)
+  // }
+  // return reversed
+
+  // Solution 3:
+  const reversed = []
+  const totalItems = str.length - 1
+  for (let i = totalItems; i = 0; i--) {
+    reversed.push(str[i])
   }
-  return reversed
+  console.log(reversed)
+
+  // console.log(reversed.join(''))
+  return reversed.join('')
 
 }
 
