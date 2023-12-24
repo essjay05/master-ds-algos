1a8620cb340fd3b781b03bab2e23a8d5f4b07071
Joy S <stepjoyous@gmail.com>
2023-12-23 23:32:11 -0800

    Creating start to hashtable's set method.

--- hashTables.js
+++ hashTables.js
@@ -1,4 +1,4 @@
-class MyHashTable {
+class HashTable {
   constructor(size) {
     this.data = new Array(size)
   }
@@ -7,8 +7,23 @@ class MyHashTable {
     let hash = 0
     for (let i = 0; i < key.length; i++) {
       hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      console.log(hash)
     }
 
     return hash
   }
+
+  set(key, value) {
+    let address = this._hash(key)
+    if (!this.data[address]) {
+      this.data[address] = []
+      this.data[address].push([key, value])
+      console.log(this.data)
+    }
+
+  }
 }
+
+const myHashTable = new HashTable(50)
+// myHashTable._hash('1232')
+myHashTable.set('grapes', 10000)
