29f5683e69e6976327dc587b528b0c0a7b60617c
Joy S <stepjoyous@gmail.com>
2023-12-25 13:39:25 -0800

    Create new file to work on firstRecurringCharacter problem.

---
+++ firstRecurringChar.js
@@ -0,0 +1,17 @@
+// Given an array find the first character that occurs more than once
+
+// Example 1:
+  // const array1 = [2,5,1,2,3,5,1,2,4]
+  // Should return 2
+
+// Example 2:
+  // const array2 = [2,1,1,2,3,5,1,2,4]
+  // Should return 1
+
+// Example 3:
+  // const array3 = [2,3,4,5]
+  // Should return undefined
+
+const findFirstRecurringChar = (array) => {
+  
+}
