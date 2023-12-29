fbb355592e2905fe8a655e7a0778cd58378ac99e
Joy S <stepjoyous@gmail.com>
2023-12-29 12:49:38 -0800

    Created file and base plan of building singlyLinkedList from scratch.

---
+++ linkedLists.js
@@ -0,0 +1,33 @@
+class MySinglyLinkedList {
+  constructor() {
+
+  }
+
+  // Methods to create from scratch:
+  // Look up
+  search() {
+
+  }
+  
+  // Add to beginning
+  prepend() {
+
+  }
+
+  // Add to end
+  append() {
+
+  }
+
+  // Insert in middle somewhere
+  insert() {
+
+  }
+
+  // Delete
+  delete() {
+
+  }
+
+
+}
