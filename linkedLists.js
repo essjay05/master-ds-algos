0c1965564fa4879939abe5a9e036833634a448a1
Joy S <stepjoyous@gmail.com>
2024-01-13 23:15:01 -0800

    Created list of base methods to create for DoublyLinkedList from scratch.

--- linkedLists.js
+++ linkedLists.js
@@ -188,4 +188,29 @@ class DoublyLinkedList {
     this.tail = this.head
     this.length = 1
   }
+
+  // Add to front of DLL: O(1)
+  prepend(value) {
+
+  }
+
+  // Add to end of DLL: O(1)
+  append(value) {
+
+  }
+
+  // Lookup value: O(n)
+  lookup(index) {
+
+  }
+
+  // Insert value at index: O(n)
+  insert(index, value) {
+
+  }
+
+  // Remove value at index: O(n)
+  remove(index) {
+    
+  }
 }
