466456c10b6811b95fce057730f0a0a28512f924
Joy S <stepjoyous@gmail.com>
2024-01-24 22:00:52 -0800

    Create methods to be created for stacks from scratch.

--- stacks.js
+++ stacks.js
@@ -1,6 +1,25 @@
 // LIFO
 class Stack {
 
+  // O(n)
+  lookup(value) {
+
+  }
+
+  // O(1)
+  push(value) {
+
+  }
+
+  // O(1)
+  pop(value) {
+
+  }
+
+  // O(1)
+  peek() {
+    
+  }
 }
 
 const myStack = new Stack(10)
